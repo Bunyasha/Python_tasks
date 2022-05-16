4673fa6bb3a76989c40ca3aed0076aed47464e19
Elena Bunyakova <elenijacinte9@gmail.com>
2022-05-16 23:26:36 +0400

    внесены правки в задачу 38

--- task038.py
+++ task038.py
@@ -1,13 +1,11 @@
 # Напишите программу, удаляющую из текста все слова, содержащие "абв".
 text=list(map(str,input('Введите текст: ').split()))
 text=' '.join(text)
-text=text.lower()
-print(type(text))
 print(text)
 
 def delete_words(text):
     text = list(filter(lambda x: 'абв' not in x, text.split()))
-    return ''.join(text)
+    return " ".join(text)
 
 
 text = delete_words(text)
